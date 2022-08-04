020a6e39c2a9f2f2ae9fc9e5aa19a98cf887fd26
bayanfuad <bayanjawawdeh9@gmail.com>
2022-08-05 01:34:45 +0300

    create server

---
+++ server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const server = express();
+const PORT = process.env.PORT;
+const weatherData = require('./data/weather.json');
+
+
+
+server.use(cors());
+
+
+server.get('/weather',(req,res)=> {
+
+    let inputCityName = req.query.searchQuery;
+    let inputLat = req.query.lat;
+    let inputLon = req.query.lon;
+
+        
+    if(!inputCityName) {
+        res.status(400).send('Please follow the documentation and fill the requierd feild as the following ?searchQuery=city name');
+    }
+
+    let result = weatherData.find(item=>inputCityName.toLowerCase() === item.city_name.toLowerCase());
+   
+    
+    try {
+        let filteredData = result.data.map(item=> new ForCast(item));
+        res.status(200).send(filteredData);
+    }catch (error) {
+        res.status(400).send('Data not found');
+    }
+
+});
+
+server.get('*',(req,res)=> {
+    res.status(500).send("Not found")
+});
+
+
+class ForCast {
+    constructor(day) {
+
+        this.date = day.valid_date;
+        this.description = day.weather.description ;
+    }
+
+};
+
+
+server.listen(PORT,()=>console.log("Server is working well!"));
